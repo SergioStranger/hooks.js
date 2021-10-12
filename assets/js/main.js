c3c95d3bac2040c85bd378a0d662efd0b3728f15
Sergio Stranger's <mail@sergios.fun>
2021-10-12 12:28:37 +0300

    add: Notification

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
+const notyf = new Notyf({
+    duration: 5000,
+    position: {
+        x: 'center',
+        y: 'top',
+    },
+    dismissible: true
+})
 const app = new Vue({
     el: "#app",
     data: {
         page: 'index',
         url: null,
         films: null,
+        errors: null,
         rating: null,
         message: null,
         watchNow: {
@@ -26,8 +35,8 @@ const app = new Vue({
         genres: function() {
             let str = '';
             for (let genre in app.films.genres) {
-                if(genre > 0) {
-                    str+= ", ";
+                if (genre > 0) {
+                    str += ", ";
                 }
                 str += app.films.genres[genre]['genre'];
             }
@@ -36,29 +45,29 @@ const app = new Vue({
         countries: function() {
             let str = '';
             for (let country in app.films.countries) {
-                if(country > 0) {
-                    str+= ", ";
+                if (country > 0) {
+                    str += ", ";
                 }
                 str += app.films.countries[country]['country'];
             }
             return str.trimEnd()
         },
         slogan: function() {
-            if(this.films.slogan == null) {
+            if (this.films.slogan == null) {
                 return '-'
             } else {
                 return this.films.slogan
             }
         },
         watchLink: function() {
-            if(this.watchNow == true) {
+            if (this.watchNow == true) {
                 return "[Подключиться к голосовому каналу для просмотра](https://discord.gg/pMzSMbnHtm)";
             } else {
                 return this.films.webUrl;
             }
         },
         historyItems: function() {
-            if(localStorage.getItem('userHistory')) {
+            if (localStorage.getItem('userHistory')) {
                 let result = localStorage.getItem('userHistory')
                 return JSON.parse(result)
             } else {
@@ -70,7 +79,7 @@ const app = new Vue({
             return date.getHours() + ':' + date.getMinutes()
         },
         watchOnTime: function() {
-            if(this.watchNow.isWatchNow && this.watchNow.watchTime) {
+            if (this.watchNow.isWatchNow && this.watchNow.watchTime) {
                 return this.timeNow
             }
         }
@@ -88,103 +97,108 @@ const app = new Vue({
             if (this.url == null || this.url == '') {
                 alert('В голове у тебя пусто!')
             } else if (typeof (this.url) != 'number') {
-                this.url = parseInt(this.url.replace(/\D+/g, ""));
+                this.url = parseInt(this.url.replace(/\D+/g, ""))
             }
 
-            if(typeof(this.url) == 'number') {
+            if (typeof(this.url) == 'number') {
                 await fetch('https://kinopoiskapiunofficial.tech/api/v2.1/films/' + this.url + "?append_to_response=RATING", {
-                    method: 'GET',
-                    headers: {
-                        'accept': 'application/json',
-                        'X-API-KEY': this.tokens.Kinopoisk.item
-                    }
-                })
-                .then(res => res.json())
-                .then(data => films = data);
+                        method: 'GET',
+                        headers: {
+                            'accept': 'application/json',
+                            'X-API-KEY': this.tokens.Kinopoisk.item
+                        }
+                    })
+                    .then(res => res.json())
+                    .then(data => films = data);
 
-                this.films = films['data'];
-                this.rating = films['rating'];
+                if (films['data'] && films['rating']) {
+                    this.films = films['data']
+                    this.rating = films['rating']
+                } else if (films['status'] == 401) {
+                    notyf.error('Ошибка токена. Проверьте правильность ввода токена и повторите попытку');
+                } else {
+                    notyf.error('Страница не найдена');
+                }
             }
         },
         async sendFilm() {
             await fetch(this.tokens.Discord.item, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        "username": "Kuнoмaнuя NEWS",
-                        "content": this.message,
-                        "embeds": [{
-                            "title": this.films.nameRu + " (" + this.films.year + ")",
-                            "color": 3368703,
-                            "description": "",
-                            "timestamp": null,
-                            "url": this.films.webUrl,
-                            "author": {
-                                "name": "Kuнoмaнuя NEWS",
-                                "url": "https://sergios.fun/"
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "username": "Kuнoмaнuя NEWS",
+                    "content": this.message,
+                    "embeds": [{
+                        "title": this.films.nameRu + " (" + this.films.year + ")",
+                        "color": 3368703,
+                        "description": "",
+                        "timestamp": null,
+                        "url": this.films.webUrl,
+                        "author": {
+                            "name": "Kuнoмaнuя NEWS",
+                            "url": "https://sergios.fun/"
+                        },
+                        "image": {
+                            "url": this.films.posterUrl
+                        },
+                        "thumbnail": {
+                            "url": this.films.posterUrl
+                        },
+                        "footer": {
+                            "text": "Возрастное ограничение: " + this.films.ratingAgeLimits + "+  |  Длительность: " + this.films.filmLength + " минут",
+                            "icon_url": "https://lh6.ggpht.com/S6_A7lzx3EfpKSBKm1Kg0N5IlHGgeja5Lb_CpPzWTB87cIsmKd70cl5GlL961ST4L9A"
+                        },
+                        "fields": [{
+                                "name": "Слоган:",
+                                "value": this.slogan,
+                                "inline": true
                             },
-                            "image": {
-                                "url": this.films.posterUrl
+                            {
+                                "name": ":film_frames: Жанр:",
+                                "value": this.genres,
+                                "inline": true
                             },
-                            "thumbnail": {
-                                "url": this.films.posterUrl
+                            {
+                                "name": ":book: Описание сюжета:",
+                                "value": this.films.description.substr(0, 1000) + "...",
+                                "inline": false
                             },
-                            "footer": {
-                                "text": "Возрастное ограничение: " + this.films.ratingAgeLimits + "+  |  Длительность: " + this.films.filmLength + " минут",
-                                "icon_url": "https://lh6.ggpht.com/S6_A7lzx3EfpKSBKm1Kg0N5IlHGgeja5Lb_CpPzWTB87cIsmKd70cl5GlL961ST4L9A"
+                            {
+                                "name": ":link: Ссылка на просмотр",
+                                "value": this.watchLink,
+                                "inline": false
                             },
-                            "fields": [
-                                {
-                                    "name": "Слоган:",
-                                    "value": this.slogan,
-                                    "inline": true
-                                },
-                                {
-                                    "name": ":film_frames: Жанр:",
-                                    "value": this.genres,
-                                    "inline": true
-                                },
-                                {
-                                    "name": ":book: Описание сюжета:",
-                                    "value": this.films.description.substr(0, 1000) + "...",
-                                    "inline": false
-                                },
-                                {
-                                    "name": ":link: Ссылка на просмотр",
-                                    "value": this.watchLink,
-                                    "inline": false
-                                },
-                                {
-                                    "name": ":map: Страна:",
-                                    "value": this.countries,
-                                    "inline": true
-                                },
-                                {
-                                    "name": ":flag_ru: Рейтинг Кинопоиск:",
-                                    "value": String(this.rating.rating),
-                                    "inline": true
-                                },
-                                {
-                                    "name": ":flag_us: Рейтинг IMDb:",
-                                    "value": String(this.rating.ratingImdb),
-                                    "inline": true
-                                },
-                            ]
-                        }]
-                    })
+                            {
+                                "name": ":map: Страна:",
+                                "value": this.countries,
+                                "inline": true
+                            },
+                            {
+                                "name": ":flag_ru: Рейтинг Кинопоиск:",
+                                "value": String(this.rating.rating),
+                                "inline": true
+                            },
+                            {
+                                "name": ":flag_us: Рейтинг IMDb:",
+                                "value": String(this.rating.ratingImdb),
+                                "inline": true
+                            },
+                        ]
+                    }]
                 })
-                // .then(res => res.json())
-                // .then(data => console.log(data));
-            
+            })
+            // .then(res => res.json())
+            // .then(data => console.log(data));
+
             // Вызываем функцию очистки полей
             this.closeFilm('success');
         },
         closeFilm(status) {
             // Сперва отправим статус в Историю
             this.saveHistory(status);
-            
+
             // Очистка полей
             this.message = null;
             this.films = null;
@@ -194,9 +208,9 @@ const app = new Vue({
             let timestamp = now.toLocaleTimeString() + " " + now.toLocaleDateString();
 
             let temp = {
-                'name': this.films.nameRu, 
-                'poster': this.films.posterUrl, 
-                'description': this.films.description, 
+                'name': this.films.nameRu,
+                'poster': this.films.posterUrl,
+                'description': this.films.description,
                 'webUrl': this.films.webUrl,
                 'status': status,
                 'time': timestamp
